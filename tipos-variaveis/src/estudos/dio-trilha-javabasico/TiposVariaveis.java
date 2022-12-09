27b290fdfc6d175661eefca4c384c4bb2536962d
CarinaBarros <83802470+CarinaGouveia@users.noreply.github.com>
2022-12-09 16:47:30 -0300

    alterações

---
+++ tipos-variaveis/src/estudos/dio-trilha-javabasico/TiposVariaveis.java
@@ -0,0 +1,18 @@
+
+public class TiposVariaveis {
+    public static void main(String[] args)  {
+
+        String meuNome = "Carina Gouveia";
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 43987562541L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+        final double VALOR_DE_PI = 3.14;
+
+       
+
+        
+    }
+}
